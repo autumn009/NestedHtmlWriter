1bd93778aede3a75807110ec9a2a0df63a6612e4
Kawamata Akira AK. <autumn@piedey.co.jp>
2016-11-04 17:08:19 +0900

    create XML Document

--- NestedHtmlWriter/arc.cmd
+++ NestedHtmlWriter/arc.cmd
@@ -1,13 +1,7 @@
-"\Program Files\NDoc\NDocConsole.exe" -documenter=MSDN -project=NestedHtmlWriter\NestedHtmlWriter.ndoc NestedHtmlWriter\bin\release\NestedHtmlWriter.xml
-del NestedHtmlWriter.lzh
-tlha a NestedHtmlWriter.lzh NestedHtmlWriter\*.cs -d
-tlha a NestedHtmlWriter.lzh NestedHtmlWriter\*.csproj -d
-tlha a NestedHtmlWriter.lzh NestedHtmlWriter\NestedHtmlWriter.ndoc -d
-tlha a NestedHtmlWriter.lzh TestNestedHtmlWriter\*.cs -d
-tlha a NestedHtmlWriter.lzh TestNestedHtmlWriter\*.csproj -d
-tlha a NestedHtmlWriter.lzh NestedHtmlWriter.sln
-tlha a NestedHtmlWriter.lzh TestNestedHtmlWriter\bin\Release\NestedHtmlWriter.dll
-tlha a NestedHtmlWriter.lzh TestNestedHtmlWriter\bin\Release\NestedHtmlWriter.xml
-tlha a NestedHtmlWriter.lzh NestedHtmlWriter.txt
-tlha a NestedHtmlWriter.lzh doc -d
-tlha a NestedHtmlWriter.lzh NestedHtmlWriter.sln
+mkdir wk
+copy help\*.chm wk
+copy NestedHtmlWriter\bin\Release\*.dll wk
+del nestedhtmlwriter.zip
+azip nestedhtmlwriter.zip wk
+rem confirm to extract
+azip -d nestedhtmlwriter.zip c:\delme
